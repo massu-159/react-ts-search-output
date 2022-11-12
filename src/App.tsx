c66d01ed5afbcb2b5b340a73012163601aa05ebc
massu-159 <magjtwjp@gmail.com>
2022-11-12 15:36:09 +0900

    fetch jsonData

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import { User } from './interfaces/User';
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [searchQuery, setSearchQuery] = useState<User[]>([]);
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data)=> setUsers(data));
+  }, []);
+
+
+  const handleSearch = () => {
+    setSearchQuery(
+      users.filter((user) => user.name.toLowerCase().includes(ref.current!.value.toLowerCase()))
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <h2>Search App</h2>
+        <input type="text" ref={ref} onChange={() => handleSearch()} />
+        <div className="content">
+          {searchQuery.length !== 0
+            ? searchQuery.map((user) => (
+              <div className="box" key={user.id}>
+                <h3>{user.name}</h3>
+                <hr />
+                <p>{user.email}</p>
+              </div>
+            ))
+            : users.map((user) => (
+              <div className="box" key={user.id}>
+                <h3>{user.name}</h3>
+                <hr />
+                <p>{user.email}</p>
+              </div>
+            )) 
+            }
+        </div>
+      </div>
     </div>
   );
 }
